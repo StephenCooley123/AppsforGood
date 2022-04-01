a2ed251f9ee0db51c46f8f54a07c7933a0b76af1
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2022-04-01 09:14:41 -0400

    Revert "hiu"
    This reverts commit 24cb4ffd

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         System.out.println("hello");
+        System.out.println("hi");
+        //if you see this the pull thingy worked
     }
 }
