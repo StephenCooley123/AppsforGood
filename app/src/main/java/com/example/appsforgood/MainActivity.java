77952fa62b74fec05f93a4309dd217d4a9fea571
StephenCooley123 <101382767+StephenCooley123@users.noreply.github.com>
2022-04-04 08:46:28 -0400

    Commit Message

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    //Commit to save local
-    //Push to save
+    //TO SAVE CHANGES TO MASTER
+    // Commit to save local
+    //Push to save to your cloud branch
+    //in menu in bottom right: master -> update
+    //master -> merge into current
+    //VCS -> Git -> Create Pull Request to send to master
+    //TO MERGE MASTER INTO CURRENT:
+    // Bottom Right Menu -> master -> update
+    //          "       -> master -> merge into current
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
