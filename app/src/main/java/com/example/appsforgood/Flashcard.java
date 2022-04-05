9a2183a9975f7a998f3b628a499244aff5788588
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2022-04-05 11:07:43 -0400

    This reverts commit 24cb4ffd

---
+++ app/src/main/java/com/example/appsforgood/Flashcard.java
@@ -0,0 +1,15 @@
+package com.example.appsforgood;
+
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.*;
+import android.os.Bundle;
+
+public class Flashcard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashcard);
+
+    }
+}
