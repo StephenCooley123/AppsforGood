e24ac5c7e288435d3c885a2e64ecebe990ef5202
Garyth <gpageviolette2005@gmail.com>
2022-04-12 19:16:19 -0400

    This is the main page with linking buttons for the flaschard page and the activity page. The Guide button allows swaps intermittently between those two pages, and the creative mode just links to the "home_page", which should be edited to be replaced with the flashcard page Donny is working on.

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appsforgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     //TO SAVE CHANGES TO MASTER
@@ -14,17 +16,27 @@ public class MainActivity extends AppCompatActivity {
     //TO MERGE MASTER INTO CURRENT:
     // Bottom Right Menu -> master -> update
     //          "       -> master -> merge into current
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("hello");
-        System.out.println("hi");
-        System.out.println("Hello this is a test!");
-        //if you see this the pull thingy worked
-        //Does this work
-        System.out.println("hello this is another test");
-        System.out.println("Stephen Test");
-        System.out.println("Stephen Test 2");
     }
+
+    public void nextWord(View v){
+        Intent intent = new Intent(this,HomePage.class);
+        intent.putExtra("condition",true);
+        startActivity(intent);
+    }
+
+    public void Description(View v){
+        Intent intent = new Intent(this,HomePage.class);
+        intent.putExtra("condition",false);
+        startActivity(intent);
+    }
+
+    public void Parental(View v){
+
+    }
+
 }
