7c898e98b0829d98355af68b6dfcb9cd8cf2c5ef
DonnyLe <98658081+DonnyLe@users.noreply.github.com>
2022-04-13 10:21:48 -0400

    This reverts commit 24cb4ffd

--- app/src/main/java/com/example/appsforgood/MainActivity.java
+++ app/src/main/java/com/example/appsforgood/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.appsforgood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     //TO SAVE CHANGES TO MASTER
@@ -14,11 +15,29 @@ public class MainActivity extends AppCompatActivity {
     //TO MERGE MASTER INTO CURRENT:
     // Bottom Right Menu -> master -> update
     //          "       -> master -> merge into current
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flashcard);
+        ImageView imageView;
+        ImageView imageView1;
+        ImageView imageView2;
+
+
+        imageView = (ImageView) findViewById(R.id.imageView1);
+        imageView.setImageResource(R.drawable.cow);
+        imageView1 = (ImageView) findViewById(R.id.imageView2);
+        imageView1.setImageResource(R.drawable.cow);
+        imageView2 = (ImageView) findViewById(R.id.imageView3);
+        imageView2.setImageResource(R.drawable.cow);
+
+        }
+
+
 
     }
 
-}
