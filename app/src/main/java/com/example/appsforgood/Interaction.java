edf8562f0eeb111f2c62d0951cb48496452c2dbe
Garyth <gpageviolette2005@gmail.com>
2022-04-13 15:51:39 -0400

    -

---
+++ app/src/main/java/com/example/appsforgood/Interaction.java
@@ -0,0 +1,34 @@
+package com.example.appsforgood;
+
+import java.util.ArrayList;
+
+public class Interaction implements Printable {
+    boolean timeout;
+    Long time;
+    ArrayList<Tap> taps;
+    ArrayList<Word> answers;
+    Word correctAnswer;
+    Word selectedAnswer;
+
+
+
+
+    public String toString() {
+
+        return "";
+    }
+
+    public static Interaction parseInteraction(String input) {
+        return null;
+    }
+
+    @Override
+    public String startKey() {
+        return "interaction{";
+    }
+
+    @Override
+    public String endKey() {
+        return "}";
+    }
+}
---
+++ app/src/main/java/com/example/appsforgood/Printable.java
@@ -0,0 +1,6 @@
+package com.example.appsforgood;
+
+public interface Printable {
+    public abstract String startKey();
+    public abstract String endKey();
+}
