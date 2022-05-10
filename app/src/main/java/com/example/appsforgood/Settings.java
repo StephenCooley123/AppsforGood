dd02d33db0e5d233dc64acc88afff271d8c13adb
Garyth <gpageviolette2005@gmail.com>
2022-05-10 18:06:08 -0400

    Some Updates

--- app/src/main/java/com/example/appsforgood/Settings.java
+++ app/src/main/java/com/example/appsforgood/Settings.java
@@ -2,7 +2,9 @@ package com.example.appsforgood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        }
+
+    public void toLists(View v){
+        Intent intent = new Intent(this, Categories.class);
+        intent.putExtra("edit",true);
+        startActivity(intent);
     }
 }
